2043fa1bebd78856ae107b1bac40c3d560aeee10
placebo-me-please <alexander.c.perry@gmail.com>
2021-03-23 19:52:21 -0600

    created a template for data to be input into

--- gladiator_battle.py
+++ gladiator_battle.py
@@ -39,23 +39,43 @@ def roll_dice(faces, quantity, option):
 	elif option == 'adv' or option == 'dis':
 		#this will likely ever only be used with a d20, but other dice may be used without limitation
 		#this chunk ignores whatever quantity is input because advantage/disadvantage die are only rolled twice
-		dual_roll[].append(randint(1, dice_dict[faces]))
-		dual_roll[].append(randint(1, dice_dict[faces]))
+		dual_roll.append(randint(1, dice_dict[faces]))
+		dual_roll.append(randint(1, dice_dict[faces]))
 		
 		#then return the lowest or highest of the two depending on the 'option' variable
 		if option == 'adv':
 			return max(dual_roll)
 		elif option == 'dis':
 			return min(dual_roll)
-		
+
+def display_character_build():
+	print(f'\u250f' + '\u2501' * 27 + '\u2513\n' \
+		'\u2503' + ' ' * 27 + '\u2503\n' \
+		'\u2503' + ' ' * 27 + '\u2503\n' \
+		'\u2503' + ' ' * 27 + '\u2503\n' \
+		'\u2503' + ' ' * 27 + '\u2503\n' \
+		'\u2503' + 'ATR' + '\u2502' + 'RAW' + '\u2502' + 'MOD' + ' ' * 2 + 'HP' + '\u2502' + 'AC' + '\u2502' + 'SP' + ' ' * 6 + '\u2503\n' \
+		'\u2503' + '\u2500' * 3 + '\u253c' + '\u2500' * 3 + '\u253c' + '\u2500' * 3  + ' ' * 2 + '\u2500' * 2 + '\u253c' + '\u2500' * 2 + '\u253c' + '\u2500' * 2  + ' ' * 6 + '\u2503\n' \
+		'\u2503' + ' ' * 3 + '\u2502' + ' ' * 3 + '\u2502' + ' ' * 7 + '\u2502' + ' ' * 2 + '\u2502' + ' ' * 8 + '\u2503\n' \
+		'\u2503' + ' ' * 3 + '\u2502' + ' ' * 3 + '\u2502' + ' ' * 19 + '\u2503\n' \
+		'\u2503' + ' ' * 3 + '\u2502' + ' ' * 3 + '\u2502' + ' ' * 5 + 'WEAPONS/ARMOR' + ' ' + '\u2503\n' \
+		'\u2503' + ' ' * 3 + '\u2502' + ' ' * 3 + '\u2502' + ' ' * 19 + '\u2503\n' \
+		'\u2503' + ' ' * 3 + '\u2502' + ' ' * 3 + '\u2502' + ' ' * 19 + '\u2503\n' \
+		'\u2503' + ' ' * 3 + '\u2502' + ' ' * 3 + '\u2502' + ' ' * 19 + '\u2503\n' \
+		'\u2503' + ' ' * 27 + '\u2503\n' \
+		'\u2517' + '\u2501' * 27 + '\u251b')
+
 #===========================================================================
 #testing
 #===========================================================================
+#this tests the printout of the character stats and information
+display_character_build()
+
 #this tests the function of a die roller
 #it takes an input of a string value corresponding to the number of faces of a die
 #it also takes the quantity of dice
 #the function then determine if the player needs advantgage, disadvantage, or the sum
 #it returns the roll or sum of the rolls if multiple dice are used
-# print(roll_dice('d4', 4, 'adv'))
-# print(roll_dice('d8', 3, 'dis'))
+# print(roll_dice('d20', 2, 'adv'))
+# print(roll_dice('d20', 2, 'dis'))
 # print(roll_dice('d100', 1, 'sum'))
